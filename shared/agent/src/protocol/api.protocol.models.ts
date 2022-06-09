89e98b2db03c938290429c175ec9f7b5651b87e6
Brian Canzanella <brian@codestream.com>
2022-06-09 12:02:16 -0400

    wip for VS CLM webview message

--- shared/agent/src/protocol/api.protocol.models.ts
+++ shared/agent/src/protocol/api.protocol.models.ts
@@ -777,6 +777,7 @@ export interface CSMePreferences {
 	demoMode?: boolean;
 	lastTeamId?: string;
 	observabilityRepoEntities?: { repoId: string; entityGuid: string }[];
+	hideCodeLevelMetricsInstructions?: boolean;
 }
 
 export interface RepoSetting {
