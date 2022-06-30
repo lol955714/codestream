d2407da989dd7f5f49743fe5cc86af0a9e6f0860
Calvin A. Allen <calvin.allen@outlook.com>
2022-06-30 13:18:45 -0400

    Change up the logging for local debugging

--- vs/src/CodeStream.VisualStudio.Core/Application.cs
+++ vs/src/CodeStream.VisualStudio.Core/Application.cs
@@ -110,19 +110,23 @@ namespace CodeStream.VisualStudio.Core {
 			// essentially a static class of semi-primitive types that all others rely on, it's ok.
 			VisualStudioDisplayName = (TryGetDisplayNameFromProcess(fileVersionInfo.FileName) ?? VisualStudioName).ToAplhaNumericPlusSafe();
 
-			string logYear = null; ;
-			if (VisualStudioVersion.Major == 15) {
-				VisualStudioVersionYear = logYear = "2017";
+			switch (VisualStudioVersion.Major) {
+				case 15:
+					VisualStudioVersionYear = "2017";
+					break;
+				case 16:
+					VisualStudioVersionYear = "2019";
+					break;
+				case 17:
+					VisualStudioVersionYear = "2022";
+					break;
 			}
-			else if (VisualStudioVersion.Major == 16) {
-				VisualStudioVersionYear = "2019";
-			}
+
 #if DEBUG
-			if (logYear != null) {
-				LogNameExtension = $"vs-{logYear}-extension.log";
-				LogNameAgent = $"vs-{logYear}-agent.log";
-			}
+			LogNameExtension = $"vs-{VisualStudioVersionYear}-extension.log";
+			LogNameAgent = $"vs-{VisualStudioVersionYear}-agent.log";
 #endif
+
 			LogPath = Path.Combine(localApplicationData, "Logs") + @"\";
 			TempDataPath = Path.Combine(tempData, "Data") + @"\";
 		}
